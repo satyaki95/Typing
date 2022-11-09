2e18ff8aacb8af0f038b3de2a111f338eb980670
satyaki95 <satyaki95@outlook.com>
2022-11-09 20:24:43 +0530

    Initialize project using Create React App

---
+++ src/Styles/global.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+body{
+    background: red;
+}
+.heading{
+    font-size: 10rem;
+}
+`;
