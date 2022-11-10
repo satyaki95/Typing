5d3ce98837b6890128fa11046488738607a5d349
satyaki95 <satyaki95@outlook.com>
2022-11-11 00:32:59 +0530

    typing box

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { GlobalStyles } from "./Styles/global";
+import TypingBox from "./Components/TypingBox";
 
 
 function App() {
   return (
     <div className="canvas">
-      <GlobalStyles/>
-      <h1 className="heading">typing</h1>
-      <div>
-        <div className='something'>
-          
-        </div>
-      
-      </div>
+      <GlobalStyles />
+      <h1>Typing Test</h1>
+      <TypingBox />
+      <h1>Footer</h1>
+
 
     </div>
+
   );
 }
 
---
+++ src/Components/TypingBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TypingBox = () => {
+  return (
+    <div>TypingBox</div>
+  )
+}
+
+export default TypingBox;
