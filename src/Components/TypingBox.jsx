3cd8e5244b648f277b7fe0966809812bf18ec7ba
satyaki95 <satyaki95@outlook.com>
2022-11-13 01:20:51 +0530

    Typing test functionalities

--- src/Components/TypingBox.jsx
+++ src/Components/TypingBox.jsx
@@ -2,20 +2,114 @@ import React,{ createRef,useEffect, useRef, useState} from 'react';
 
 const TypingBox = ({words}) => {
 
-  const [currWordIndex, setcurrWordIndex] = useState(0);
-  const [currCharIndex, setcurrCharIndex] = useState(0);
+  const [currWordIndex, setCurrWordIndex] = useState(0);
+  const [currCharIndex, setCurrCharIndex] = useState(0);
+  const [countDown, setCountDown] = useState(15);
+  const [testStart, setTestStart] = useState(false);
+  const [testOver, setTestOver] = useState(false);
 
   const inputTextRef = useRef(null);
 
   const wordSpanRef = Array(words.length).fill(0).map(i=>createRef(null));
 
+
+  const startTimer = () => {
+      const intervalId = setInterval(timer, 1000);
+
+      function timer(){
+        setCountDown((prevCountDown)=>{
+
+          if(prevCountDown===1){
+            clearInterval(intervalId);
+            setCountDown(0);
+            setTestOver(true);
+          }
+          else{
+            return prevCountDown-1;
+          }
+          
+        });
+      }
+  }
+
   const handleKeyDown = (e) =>{
+
+
+    if(!testStart){
+      startTimer();
+      setTestStart(true);
+    }
+    
+
     let allChildrenSpans = wordSpanRef[currWordIndex].current.childNodes;
    
     
 // Space
 
+if(e.keyCode===32){
+
+  // removing the cursor from the word
+  if(allChildrenSpans.length<=currCharIndex){
+    allChildrenSpans[currCharIndex-1].classList.remove('right');
+    // or
+    //allChildrenSpans[currCharIndex-1].className = allChildrenSpans[currCharIndex-1].className.replace('right','');
+  }
+
+  else{
+    allChildrenSpans[currCharIndex].className = allChildrenSpans[currCharIndex].className.replace('current','');
+  }
+
+// Add cursor to the next word
+
+wordSpanRef[currWordIndex+1].current.childNodes[0].className = 'char current';
+
+  setCurrWordIndex(currWordIndex+1);
+  setCurrCharIndex(0);
+
+  return;
+
+}
+
 //Backpace
+
+if(e.keyCode===8){
+  if(currCharIndex!==0){
+
+    if(currCharIndex===allChildrenSpans.length){
+
+      if(allChildrenSpans[currCharIndex-1].className.includes('extra')){
+        allChildrenSpans[currCharIndex-1].remove();
+        allChildrenSpans[currCharIndex-2].className+= ' right';
+      }
+      else{
+        allChildrenSpans[currCharIndex-1].className = 'char current';
+      }
+
+      
+      setCurrCharIndex(currCharIndex-1);
+      return;
+    }
+
+    allChildrenSpans[currCharIndex].className = 'char';
+    allChildrenSpans[currCharIndex-1].className = 'char current';
+    setCurrCharIndex(currCharIndex-1);
+  }
+
+  return;
+}
+
+if(currCharIndex===allChildrenSpans.length){
+
+  let newSpan = document.createElement('span');
+  newSpan.innerText = e.key;
+  newSpan.className = 'char incorrect right extra';
+  allChildrenSpans[currCharIndex-1].className = allChildrenSpans[currCharIndex-1].className.replace('right',''); 
+
+
+  wordSpanRef[currWordIndex].current.append(newSpan);
+  setCurrCharIndex(currCharIndex+1);
+  return;
+}
 
 
 // Correct and incorrect characters
@@ -28,7 +122,19 @@ const TypingBox = ({words}) => {
       allChildrenSpans[currCharIndex].className='char incorrect';
     }
 
-    setcurrCharIndex(currCharIndex+1);
+    if(currCharIndex+1=== allChildrenSpans.length){
+      allChildrenSpans[currCharIndex].className+= ' right';
+    }
+
+    else{
+      allChildrenSpans[currCharIndex+1].className = 'char current';
+    }
+
+    
+    setCurrCharIndex(currCharIndex+1);
+
+
+
   }
 
   const focusInput = ()=>{
@@ -38,22 +144,27 @@ const TypingBox = ({words}) => {
   useEffect(()=>{
     
     focusInput();
+    wordSpanRef[0].current.childNodes[0].className = 'char current';
   },[]);
 
 
   return (
     <div>
-      <div className='type-box'>
-        <div className='words'>
-          {words.map((word,index)=>(
-            <span className='word'ref={wordSpanRef[index]}>
-                {word.split("").map((char,idx)=>(
-                  <span className='char'>{char}</span>
+      <h1>{countDown}</h1>
+      {testOver?(<h1>Test Over</h1>):(
+              <div className='type-box' onClick={focusInput}>
+              <div className='words'>
+                {words.map((word,index)=>(
+                  <span className='word'ref={wordSpanRef[index]} key={index}>
+                      {word.split("").map((char,idx)=>(
+                        <span className='char'key={`char${idx}`}>{char}</span>
+                      ))}
+                  </span>
                 ))}
-            </span>
-          ))}
-        </div>
-      </div>
+              </div>
+            </div>
+      )}
+
       <input 
       type='text' 
       className='hidden-input'
--- src/Styles/global.js
+++ src/Styles/global.js
@@ -38,7 +38,7 @@ body{
     font-family: 'Kalam', cursive;
 }
 .words{
-    font-size:30px;
+    font-size:25px;
     display:flex;
     flex-wrap:wrap;
     align-content:center;
@@ -57,5 +57,31 @@ body{
 }
 .incorrect{
     color:red;
+}
+.current{
+    border-left:1px solid;
+    animation: blinking 2s infinite;
+    animation-timing-function: ease;
+
+    @keyframes blinking{
+        0% {border-left-color:#fff;}
+        25% {border-left-color:black;}
+        50% {border-left-color:#fff;}
+        75% {border-left-color:black;}
+        100% {border-left-color:#fff;}
+    }
+}
+.right{
+    border-right:1px solid;
+    animation: blinking 2s infinite;
+    animation-timing-function: ease;
+
+    @keyframes blinkingRight{
+        0% {border-right-color:#fff;}
+        25% {border-right-color:black;}
+        50% {border-right-color:#fff;}
+        75% {border-right-color:black;}
+        100% {border-right-color:#fff;}
+    }
 }
 `;
